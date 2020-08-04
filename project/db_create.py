f95ef078853040cfc6f6ead8219eb79240a59579
Earl Pearl <earlpearl17@gmail.com>
2020-08-04 05:32:22 -0400

    Replaced SQLite3 db using SQLAlchemy

--- project/db_create.py
+++ project/db_create.py
@@ -1,25 +1,19 @@
 # project / db_create.py
 
-import sqlite3
-from _config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
-    # get a cursor object used to execute SQL commands
-    c = connection.cursor()
+#import pdb
 
-    # create the table
-    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL,
-        status INTEGER NOT NULL)""")
+#pdb.set_trace()
 
-    # insert dummy data into the table
-    c.execute(
-        'INSERT INTO tasks (name, due_date, priority, status)'
-        'VALUES("Finish this tutorial", "08/03/2020", 10, 1)'
-        )
-    c.execute(
-        'INSERT INTO tasks (name, due_date, priority, status)'
-        'VALUES("Finish Real Python Course 2", "09/13/2020", 10, 1)'
-        )
+# create the database and the db table
+db.create_all()
 
+# insert data
+db.session.add(Task("Finish SQLAlchemy tutorial", date(2020, 8, 4), 10, 1))
+db.session.add(Task("Finish Real Python Part 2", date(2020, 9, 17), 10, 1))
 
+# commit the changes
+db.session.commit()
